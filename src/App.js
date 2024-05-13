a647f097b2d066c6ff3822114c6e2ba69264d484
Mariia Melnik <141711394+FormulujMario@users.noreply.github.com>
2024-05-13 21:37:34 +0200

    Initialize footer

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import Header from "./components/header/Header.js";
+import PageTitle from "./components/PageTitle.js";
+import SliderMainPage from "./components/slider/SliderMainPage.js";
+import Bestsellers from "./components/bestsellers/Bestsellers.js";
+import LinkToAboutUs from "./components/link-to-about-us/LinkToAboutUs.js";
+import KabinetProducts from "./components/KabinetProducts.js";
+import KabinetLounge from "./components/kabinet-lounge/KabinetLounge.js";
+import LatestNews from "./components/LatestNews.js";
+import Footer from "./components/footer/Footer.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <PageTitle title="CRAFT BREWERY" />
+        <SliderMainPage />
+        <Bestsellers />
+        <LinkToAboutUs />
+        <KabinetProducts />
+        <KabinetLounge />
+        <LatestNews />
+        <Footer/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
