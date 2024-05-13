acc07a63008c197147206693bcac537691f3ff0e
Mariia Melnik <141711394+FormulujMario@users.noreply.github.com>
2024-05-13 21:37:33 +0200

    Initialize kabinet-products

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./components/header/Header.js";
+import PageTitle from "./components/PageTitle.js";
+import SliderMainPage from "./components/slider/SliderMainPage.js";
+import Bestsellers from "./components/bestsellers/Bestsellers.js";
+import LinkToAboutUs from "./components/link-to-about-us/LinkToAboutUs.js";
+import KabinetProducts from "./components/KabinetProducts.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <PageTitle title="CRAFT BREWERY" />
+        <SliderMainPage />
+        <Bestsellers />
+        <LinkToAboutUs />
+        <KabinetProducts/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
