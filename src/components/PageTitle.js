ac430191a69e861cdee44961611405d41362f2aa
FormulujMario <msh89.mail@gmail.com>
2024-06-30 23:27:39 +0200

    add desktop version

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,20 +1,5 @@
-import scssVars from "./../scss/App.scss";
-
 const PageTitle = ({ title }) => {
-  const windowInnerWidth = document.documentElement.clientWidth;
-  if (windowInnerWidth <= scssVars.breakpoint_xl) {
-    return <h1>{title}</h1>;
-  } else {
-    return title.length > 12 ? (
-      <h1 style={{ justifyContent: "space-between" }}>
-        {title.split(" ").map((word) => {
-          return <span key={word}>{word}</span>;
-        })}
-      </h1>
-    ) : (
-      <h1 style={{ justifyContent: "center" }}>{title}</h1>
-    );
-  }
+  return <h1>{title}</h1>;
 };
 
 export default PageTitle;
